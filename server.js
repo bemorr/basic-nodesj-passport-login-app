5df166db88379ba56e43053bf7a4653adfbc079d
bemorr <benmorrisldnuk@gmail.com>
2020-02-12 22:39:26 +0100

    adds register, login and  / routes + EJS engine in server.js + Views for the above listed routes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.set("view-engine", "ejs");
+
+app.get("/", (req, res) => {
+	res.render("index.ejs", { name: "Ben" });
+});
+
+app.get("/register", (req, res) => {
+	res.render("register.ejs");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login.ejs");
+});
+
+app.listen(3001);
